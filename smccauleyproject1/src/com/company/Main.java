bf9e0a741f4732c5a92cc2d987fbdb6dc4c586bf
seth7777123 <34112977+seth7777123@users.noreply.github.com>
2022-02-02 20:46:27 -0500

    Add files via upload
    i hope

---
+++ smccauleyproject1/src/com/company/Main.java
@@ -0,0 +1,44 @@
+package com.company;
+
+
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import java.util.Scanner;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class Main {
+
+
+
+    public static void main(String[] args) {
+        HttpClient client = HttpClient.newHttpClient();
+
+        secret sec = new secret();
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create("https://imdb-api.com/en/API/Top250TVs/"+ sec.api_key)).build();
+
+        client.sendAsync(request, HttpResponse.BodyHandlers.ofString()).thenApply(HttpResponse::body).thenAccept(System.out::println).join();
+
+
+    }
+
+
+    public static void parse(String responseBody) {
+
+        JSONArray schools = new JSONArray(responseBody);
+        for (int i = 0; i < schools.length(); i++) ;
+        {
+
+
+
+
+
+
+        }
+    }
+}
+
+
---
+++ smccauleyproject1/src/com/company/secret.java
@@ -0,0 +1,6 @@
+package com.company;
+
+public class secret<string> {
+
+     public String api_key="k_a8m5anc7";
+}
